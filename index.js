94f63abae6382a4877d76338a156db4fb0d22ea3
megumihfu <megumihfu@gmail.com>
2023-05-31 22:53:21 +0300

    création d'un bouton qui redirige vers la page create account

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const port = 3000;
+const host = 'localhost'
 
 const server = http.createServer((req, res) => {
   const filePath = path.join(__dirname, 'index.html');
@@ -9,7 +10,7 @@ const server = http.createServer((req, res) => {
   fs.readFile(filePath, (err, content) => {
     if (err) {
       res.writeHead(500);
-      res.end('Une erreur est survenue sur le serveur.');
+      res.end('An error has occured');
     } else {
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end(content, 'utf-8');
@@ -19,5 +20,5 @@ const server = http.createServer((req, res) => {
 
 
 server.listen(port, () => {
-  console.log(`App is listenning on port ${port}`);
+  console.log(`Server is running on http://${host}:${port}`);
 });
