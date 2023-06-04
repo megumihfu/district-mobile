3b3516aa7fe3ab572282603f71b3c03d3e0af252
megumihfu <megumihfu@gmail.com>
2023-06-04 14:37:26 +0300

    creation de menu déroulant pour les filtres + ajout des choix user

--- index.js
+++ index.js
@@ -3,22 +3,26 @@ const fs = require('fs');
 const path = require('path');
 const port = 3000;
 const host = 'localhost'
+const data = fs.readFileSync('./database.json', 'utf8');
+
+//document.getElementById("Brand").innerHTML = html;
 
 const server = http.createServer((req, res) => {
-  const filePath = path.join(__dirname, 'index.html');
+const filePath = path.join(__dirname, 'index.html');
   
-  fs.readFile(filePath, (err, content) => {
+    fs.readFile(filePath, (err, content) => {
     if (err) {
-      res.writeHead(500);
-      res.end('An error has occured');
+        res.writeHead(500);
+        res.end('An error has occured');
     } else {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(content, 'utf-8');
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content, 'utf-8');
     }
-  });
+    });
 });
 
 
 server.listen(port, () => {
   console.log(`Server is running on http://${host}:${port}`);
 });
+
