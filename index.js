8e050a62bc3caa4a3406357f5eea2f26387d30aa
megumihfu <megumihfu@gmail.com>
2023-06-12 20:07:05 +0300

    fonctionne pour la première partie de la page xiaomi 13 -> affichage ok

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+
 const port = 3000;
 const host = 'localhost'
 const data = fs.readFileSync('./database.json', 'utf8');
